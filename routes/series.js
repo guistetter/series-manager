819bce9e31618e41c3600f3752e28e8b82bb9c0c
guistetter <guilherme_stetter@hotmail.com>
2020-08-03 15:03:47 -0300

    corrigindo erro de rota, posta/get

--- routes/series.js
+++ routes/series.js
@@ -7,7 +7,7 @@ const models = {
   Serie
 }
 router.get("/", seriesController.index.bind(null, models))
-router.post("/nova", seriesController.novaForm)
+router.get("/nova", seriesController.novaForm)
 router.post("/nova", seriesController.novaProcess.bind(null, models))
 
 module.exports = router
